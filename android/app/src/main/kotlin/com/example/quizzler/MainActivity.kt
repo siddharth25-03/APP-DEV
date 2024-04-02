e31727c8488f22f53fe960439829cfd82fd53579
siddharth25-03 <siddharththikekar14@gmail.com>
2024-04-03 00:53:32 +0530

    final

--- android/app/src/main/kotlin/com/example/quizzler/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.quizzler
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/question.dart
+++
@@ -1,9 +0,0 @@
-class Question {
-  String question=' ';
-  bool answer=false;
-
-  Question(String? q, bool? ans) {
-    question = q ?? 'Default question'; // Providing a default value if q is null
-    answer = ans ?? false; // Providing a default value if ans is null
-  }
-}
